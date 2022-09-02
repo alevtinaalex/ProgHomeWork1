14117f3791f6e5aeafc9011d17f27c910d8e845f
Alevtina <_@email.comgit>
2022-09-02 21:31:23 +0300

    Add chet and nechet

---
+++ task3_chet_nechet/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число и нажмите Enter ");
+int Chislo = Convert.ToInt32(Console.ReadLine());
+
+if (Chislo % 2 == 0)
+ { 
+   Console.Write( "Это четное число");
+ }
+else 
+  {  
+    Console.Write("Это нечетное число");
+  }
+
