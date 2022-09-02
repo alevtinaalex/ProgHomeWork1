585f9e004c4b3f766661ef08cc7f123971f70764
Alevtina <_@email.comgit>
2022-09-02 20:30:59 +0300

    Add min max iz 2 chisel

---
+++ task1_chis2_min_max/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число a и нажмите Enter ");
+int FirstA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b и нажмите Enter ");
+int SecondB = Convert.ToInt32(Console.ReadLine());
+
+int max, min;
+if (FirstA > SecondB)
+ { 
+    max = FirstA;
+    min = SecondB;
+ }
+else 
+  {  
+    max = SecondB;
+    min = FirstA;
+  }
+  Console.Write("Максимальное число ");
+  Console.WriteLine(max);
+  Console.Write("Минимальное число ");
+  Console.WriteLine(min);
