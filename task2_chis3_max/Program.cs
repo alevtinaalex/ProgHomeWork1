276465af05b9491bea913b14235a93ea2bee96a9
Alevtina <_@email.comgit>
2022-09-02 21:21:21 +0300

    Add max iz chisel3

---
+++ task2_chis3_max/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число a и нажмите Enter ");
+int FirstA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b и нажмите Enter ");
+int SecondB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c и нажмите Enter ");
+int ThirdC = Convert.ToInt32(Console.ReadLine());
+
+int max = FirstA;
+
+if (SecondB > max)
+  {  
+    max = SecondB;
+  }
+if ( ThirdC > max)
+ {
+   max = ThirdC;
+ }
+  Console.Write("Максимальное число ");
+  Console.Write(max); 
+
+
